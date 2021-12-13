127b5c30cfaa9dd7874ec3877a8050c77ed1eae2
Amit Tilakchand yele <amit@codexxa.in>
2021-12-13 14:55:03 +0530

    changes

--- application/models/Staff_model.php
+++ application/models/Staff_model.php
@@ -163,8 +163,8 @@ class Staff_model extends MY_Model
 
     public function add($data)
     {
-        $this->db->trans_start(); # Starting Transaction
-        $this->db->trans_strict(false); # See Note 01. If you wish can remove as well
+        // $this->db->trans_start(); # Starting Transaction
+        // $this->db->trans_strict(false); # See Note 01. If you wish can remove as well
         //=======================Code Start===========================
         if (isset($data['id'])) {
             $this->db->where('id', $data['id']);
@@ -185,17 +185,18 @@ class Staff_model extends MY_Model
         }
         //======================Code End==============================
 
-        $this->db->trans_complete(); # Completing transaction
-        /*Optional*/
+        // $this->db->trans_complete(); # Completing transaction
+        // /*Optional*/
 
-        if ($this->db->trans_status() === false) {
-            # Something went wrong.
-            $this->db->trans_rollback();
-            return false;
+        // if ($this->db->trans_status() === false) {
+        //     # Something went wrong.
+        //     $this->db->trans_rollback();
+        //     return false;
 
-        } else {
-            return $id;
-        }
+        // } else {
+           
+        // }
+        return $id;
     }
 
     public function update($data)
