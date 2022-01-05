718fc98a9ea4541499937082a394efbf2139b20e
Amit Tilakchand yele <amit@codexxa.in>
2022-01-05 22:16:19 +0530

    changes

--- application/models/Class_model.php
+++ application/models/Class_model.php
@@ -254,7 +254,11 @@ class Class_model extends MY_Model {
     }
 
     public function getClassTeacher() {
-        $query = $this->db->query('SELECT class_teacher.*,classes.class,sections.section FROM `class_teacher` INNER JOIN classes on classes.id=class_teacher.class_id INNER JOIN sections on sections.id=class_teacher.section_id where class_teacher.session_id="' . $this->current_session . '" GROUP BY class_teacher.class_id , class_teacher.section_id ORDER by length(classes.class), classes.class');
+
+        $admin = $this->session->userdata('admin');
+        $id = $admin['sch_id'];
+
+        $query = $this->db->query('SELECT class_teacher.*,classes.class,sections.section FROM `class_teacher` INNER JOIN classes on classes.id=class_teacher.class_id INNER JOIN sections on sections.id=class_teacher.section_id where class_teacher.session_id="' . $this->current_session . '" AND sections.sch_id="' .  $id . '"  GROUP BY class_teacher.class_id , class_teacher.section_id ORDER by length(classes.class), classes.class');
 
         //     $query = $this->db->query('SELECT distinct class_id AS class_id ,section_id,
         //  (SELECT C.class FROM classes C WHERE C.ID = CT.CLASS_ID) class,
